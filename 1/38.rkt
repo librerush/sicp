1a9d644a41e9108a02d865f40cd4b911b4ddbba0
Dias Kozhabay <librerush@tutanota.com>
2019-09-24 19:41:45 +0600

    Fix 1/37.rkt and Add 1/38.rkt

--- 1/38.rkt
+++ 1/38.rkt
@@ -1,6 +1,6 @@
 #lang sicp
 
-; ex 1.37
+; ex 1.38
 
 (define (cont-frac n d k)
   (define (loop i)
@@ -19,19 +19,16 @@
             (iter (dec i) (/ nk (+ dk result)))))))
   (iter k 0))
 
-; How large must you make
-; k in order to get an approximation that is accurate to
-; 4 decimal places?
-; when k = 12
+(define (e-value k)
+  (+ 2.0 (cont-frac
+           (lambda (i) 1.0)
+           (lambda (i)
+              (if (= (remainder i 3) 2)
+                  (/ (+ i 1) 1.5)
+                  1.0))
+           k)))
 
-; (cont-frac (lambda (i) 1.0)
-;            (lambda (i) 1.0)
-;            12)
-
-; 0.6180257510729613 
-
-; (cont-frac-iter (lambda (i) 1.0)
-;                 (lambda (i) 1.0)
-;                 12)
+; (e-value 22)
+; 2.7182818284590455
 
 
