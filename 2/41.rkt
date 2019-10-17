c70c54ff3bd3b3aa523140f49246e2f6169971d8
Dias Kozhabay <librerush@tutanota.com>
2019-10-17 18:59:06 +0600

    Add 2/41.rkt

---
+++ 2/41.rkt
@@ -0,0 +1,40 @@
+#lang sicp
+
+; ex 2.41
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (unique-triples n)
+  (flatmap (lambda (i)
+             (flatmap (lambda (j)
+                        (map (lambda (k) (list i j k))
+                             (enumerate-interval 1 (- j 1))))
+                      (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (triples-of-sum n s)
+  (filter (lambda (x) (= (accumulate + 0 x) s))
+          (unique-triples n)))
+
+; (triples-of-sum 5 8)
+; ((4 3 1) (5 2 1)) 
+
