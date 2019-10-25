1fe678719c8a2ed9fd26c32bdacc5774ab784391
Dias Kozhabay <librerush@tutanota.com>
2019-10-25 23:02:51 +0600

    Add 2/50.rkt

---
+++ 2/50.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+(#%require sicp-pict)
+
+; ex 2.50
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(paint einstein)
