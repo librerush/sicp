2721b0967a72ca3d425915cc60133a27131f4f70
Dias Kozhabay <librerush@tutanota.com>
2020-01-27 16:11:45 +0600

    Add 2/68.rkt

---
+++ 2/68.rkt
@@ -0,0 +1,98 @@
+#lang sicp
+
+; ex 2.68
+
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? object) (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)    ; symbol
+                               (cadr pair))  ; frequency
+                    (make-leaf-set (cdr pairs))))))
+
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree
+                    (make-leaf 'D 1)
+                    (make-leaf 'C 1)))))
+
+(define sample-message
+  '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;; (decode sample-message sample-tree)
+;; (A D A B B C A)
+
+;; (display sample-tree) (newline)
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+  (define (encode-symbol-aux symbol tree bag)
+    ;; (display symbol) (display " ") (display tree) (display " ") (display bag) (newline)
+    (cond ((null? tree) bag)
+          ((leaf? tree) bag)
+          (else (let ((lft (left-branch tree))
+                      (rgt (right-branch tree)))
+                  (if (and (leaf? lft) (eq? symbol (symbol-leaf lft)))
+                      (cons 0 bag)
+                      (encode-symbol-aux symbol rgt (cons 1 bag)))))))
+
+  (reverse (encode-symbol-aux symbol tree '())))
+
+;; (encode '(A D A B B C A) sample-tree)
+;; (0 1 1 0 0 1 0 1 0 1 1 1 0)
