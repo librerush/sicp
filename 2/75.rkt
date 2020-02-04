b04e730a2daad12958eac6be494c56733bdb0910
Dias Kozhabay <librerush@tutanota.com>
2020-02-04 19:35:10 +0600

    Add 2/75.rkt

---
+++ 2/75.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+; ex 2.75
+
+(define (make-from-mag-ang x y)
+  (define (dispatch op)
+    (cond ((eq? op 'magnitude) x)
+          ((eq? op 'angle) y)
+          ((eq? op 'real-part) (* x (cos y)))
+          ((eq? op 'imag-part) (* x (sin y)))
+          (else (error "Unknown op: MAKE-FROM-MAG-ANG" op))))
+  dispatch)
