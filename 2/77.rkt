f75af1e1c61d6d8170261dc4cada5a955929fed7
Dias Kozhabay <librerush@tutanota.com>
2020-02-08 19:19:09 +0600

    Add 2/77.rkt and 2/78.rkt

---
+++ 2/77.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+
+; ex 2.77
+
+;; (magnitude z) ->
+;; (apply-generic 'magnitude z) ->
+;; (magnitude z) -> ;; in rectangular package
+;; (sqrt (+ (square (real-part z))
+;;          (square (imag-part z))))
