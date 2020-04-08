0f0918a86b723b61a7b913c16f041b1ab01a6cae
Dias Kozhabay <librerush@tutanota.com>
2020-04-08 23:45:59 +0600

    Add 3/15.rkt

---
+++ 3/15.rkt
@@ -0,0 +1,38 @@
+#lang sicp
+
+;; ex 3.15
+
+(define x (list 'a 'b))
+(define z1 (cons x x))
+
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+
+(define (set-to-wow! x) (set-car! (car x) 'wow) x)
+
+;; z1
+;; ((a b) a b)
+;; (set-to-wow! z1)
+;; ((wow b) wow b)
+
+;; z2
+;; ((a b) a b)
+;; (set-to-wow! z2)
+;; ((wow b) (a b))
+
+;; z1 --> .-----.
+;;        :     :
+;;        v     v
+;; x ---> .-----.---->.---->'/
+;;        :           :
+;;        v           v
+;;      'wow         'b
+
+;; z2 --> .-----.---->.-----.----->.-----'/
+;;        :           :            :
+;;        :           v            v
+;;        :          'a           'b
+;;        :                        ^
+;;        :         'wow           :
+;;        :           ^            :
+;;        :           :            :
+;;        :---------->.-----.----->.-----'/
