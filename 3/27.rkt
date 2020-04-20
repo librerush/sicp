849e4a06339597360fb5b772f9ed0bb9fb2915fe
Dias Kozhabay <librerush@tutanota.com>
2020-04-20 17:14:32 +0600

    Add 3/27.rkt

---
+++ 3/27.rkt
@@ -0,0 +1,10 @@
+#lang sicp
+
+
+;; memo-fib stores every Fibonacci number between 0..n in the table
+;; so we can reuse previously computed values.
+
+
+;; (memoize fib) is not the same as memo-fib.
+;; ((memoize fib) n) generates tree recursion, and stores only
+;; n-th Fibonacci number.
