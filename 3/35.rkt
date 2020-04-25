bc9d441ea0a0667da0863f8c530f3c4ead8e87da
Dias Kozhabay <librerush@tutanota.com>
2020-04-25 22:16:33 +0600

    Add 3/35.rkt

---
+++ 3/35.rkt
@@ -0,0 +1,38 @@
+#lang sicp
+
+;; ex 3.35
+
+(#%require "constraints.rkt")
+
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+	(if (< (get-value b) 0)
+	    (error "square less than 0: SQUARER"
+		   (get-value b))
+	    (set-value! a
+			(sqrt (get-value b))
+			me))
+	(set-value! b
+		    (* (get-value a) (get-value a))
+		    me)))
+  (define (process-forget-value)
+    (forget-value! b me)
+    (forget-value! a me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value) (process-new-value))
+	  ((eq? request 'I-lost-my-value) (process-forget-value))
+	  (else (error "Unknown request: SQUARER" request))))
+  (connect a me)
+  (connect b me)
+  me)
+
+
+;; (define a (make-connector))
+;; (define b (make-connector))
+
+;; (probe "a" a)
+;; (probe "b" b)
+;; (set-value! b 4 'user)
+;; (squarer a b)
