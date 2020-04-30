87fc8a8e9b7797ee220cd14173992ab2be05d018
Dias Kozhabay <librerush@tutanota.com>
2020-05-01 01:47:49 +0600

    Add 3/42.rkt

---
+++ 3/42.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+;; ex 3.42
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw (protected withdraw))
+	  (protected-deposit (protected deposit)))
+      (define (dispatch m)
+	(cond ((eq? m 'withdraw) protected-withdraw)
+	      ((eq? m 'deposit) protected-deposit)
+	      ((eq? m 'balance) balance)
+	      (else (error "Unknown request -- MAKE-ACCOUNT"
+			   m))))
+      dispatch)))
+
+;; a. Yes, it's safe.
+;; b. There is no difference in two versions of make-account
