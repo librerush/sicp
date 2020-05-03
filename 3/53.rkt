240ae6471ad4180bf8f96001bea4ceca790ead1a
Dias Kozhabay <librerush@tutanota.com>
2020-05-03 19:14:43 +0600

    Add 3/53.rkt

---
+++ 3/53.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+;; ex 3.53
+
+(#%require "streams.rkt")
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define s (cons-stream 1 (add-streams s s)))
+
+;; (1 2 4 8 16 32 ...)
+;; 2^n
