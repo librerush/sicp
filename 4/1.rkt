e3cc43e3cbbf8ec6970da027facc3897b4704a3f
Dias Kozhabay <librerush@tutanota.com>
2020-05-23 18:00:49 +0600

    Chapter 4. Add 4/1.rkt

---
+++ 4/1.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+;; ex 4.1
+
+(define (list-of-values-left exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((first (eval (first-operand exps) env)))
+	(let ((rest (list-of-values (rest-operands exps) env)))
+	  (cons first
+		rest)))))
+
+(define (list-of-values-right exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((rest (list-of-values (rest-operands exps) env)))
+	(let ((first (eval (first-operand exps) env)))
+	  (cons first
+		rest)))))
