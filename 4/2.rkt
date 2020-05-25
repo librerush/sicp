56d5008d8406d5fbf45c49fad06c2657026392be
Dias Kozhabay <librerush@tutanota.com>
2020-05-26 02:03:08 +0600

    Add 4/2.rkt

---
+++ 4/2.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+;; ex 4.2
+
+;; a. (define x 3)
+;; (apply (eval 'define env)
+;;        (list-of-values '(x 3) env)
+;; Unknown expression type -- EVAL define
+
+;; b.
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+	((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+	((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env)) 
+        ((begin? exp) 
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        (else
+         (error "Unknown expression type -- EVAL" exp))))
+
+(define (application? exp)
+  (tagged-list? exp 'call))
