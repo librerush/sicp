7eae57eed48733ac9205bffc5275864c15b0506e
Dias Kozhabay <librerush@tutanota.com>
2020-06-10 00:28:21 +0600

    Add 4/23.rkt

---
+++ 4/23.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+;; ex 4.23
+
+;; One expression
+;; Alyssa: calls procedure with environment
+;; Text: returns procedure which takes environment
+
+;; Two expressions
+;; Alyssa: first procedure called with environment
+;; and then second procedure
+;; Text: returns procedure which takes environment
+;; and call two procedures
