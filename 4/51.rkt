fb466fb2d83b33884414ed583169dda36a1ab564
Dias Kozhabay <librerush@tutanota.com>
2020-06-27 00:42:14 +0600

    Add 4/51.rkt

---
+++ 4/51.rkt
@@ -0,0 +1,20 @@
+#lang sicp
+
+;; ex 4.51
+
+(define (permanent-set? exp)
+  (tagged-list? exp 'permanent-set!))
+
+(define (analyze-perm-set exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+                 (set-variable-value! var val env)
+                 (succeed 'ok
+                          (lambda ()
+                            (fail2))))
+             fail))))
+
+;; If we use set! count variable always be 1.
