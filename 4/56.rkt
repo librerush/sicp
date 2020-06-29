233ad2fc72e13d683a8b231546d4dba16959415a
Dias Kozhabay <librerush@tutanota.com>
2020-06-30 00:00:27 +0600

    Add 4/56.rkt

---
+++ 4/56.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+;; ex 4.56
+
+;; a.
+;; (and (supervisor ?name (Bitdiddle Ben))
+;;      (address ?name ?address))
+
+;; b.
+;; (and (salary (Bitdiddle Ben) ?ben-salary)
+;;      (salary ?name ?salary)
+;;      (lisp-value > ?ben-salary ?salary))
+
+;; c.
+;; (and (supervisor ?name ?someone)
+;;      (not (job ?someone (computer . ?divison)))
+;;      (job ?someone ?job))
