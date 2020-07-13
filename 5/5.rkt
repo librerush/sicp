f04731f2d1fa0559a8243c10fd4b79ffd55415f2
Dias Kozhabay <librerush@tutanota.com>
2020-07-14 01:21:44 +0600

    Add 5/5.rkt

---
+++ 5/5.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+;; ex 5.5
+
+#|
+   (factorial 3)
+
+                     | 2          | 
+                     | after-fact |
+   | 3         | --> | 3          | --> | 3         |
+   | fact-done |     | fact-done  |     | fact-done |
+
+|#
