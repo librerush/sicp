bc168ed16871ec7049231cd92fd4b5cd610400f9
Dias Kozhabay <librerush@tutanota.com>
2020-07-13 23:25:50 +0600

    Add 5/4.rkt

---
+++ 5/4.rkt
@@ -0,0 +1,34 @@
+#lang sicp
+
+;; ex 5.4
+
+;; a. recursive
+'(controller
+  (assign continue (label expt-done))
+  expt-loop
+  (test (op =) (reg n) (const 0))
+  (branch (label base-case))
+  (save continue)
+  (assign n (op -) (reg n) (const 1))
+  (assign continue (label after-expt))
+  (goto (label expt-loop))
+  after-expt
+  (restore continue)
+  (assign val (op *) (reg b) (reg val))
+  (goto (reg continue))
+  base-case
+  (assign val (const 1))
+  (goto (label continue))
+  expt-done)
+
+;; b. iterative
+'(controller
+  (assign counter (reg n))
+  (assign product (const 1))
+  expt-loop
+  (test (op =) (reg counter) (const 0))
+  (branch (label expt-done))
+  (assign counter (op -) (reg counter) (const 1))
+  (assign product (op *) (reg b) (reg product))
+  (goto (label expt-loop))
+  expt-done)
