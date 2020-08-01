ab4d826aae27c8dc4bb43dc590a93cf67dbb4d6a
Dias Kozhabay <librerush@tutanota.com>
2020-08-02 01:54:16 +0600

    Add 5/28.rkt

---
+++ 5/28.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+;; ex 5.28
+
+#|
+  Recursive factorial
+  n  | max depth | number of pushes
+  1  | 11        | 18
+  2  | 19        | 52
+  3  | 27        | 86
+  4  | 35        | 120
+
+  Iterative factorial
+  n  | max depth | number of pushes
+  1  | 17        | 70
+  2  | 20        | 107
+  3  | 23        | 144
+  4  | 26        | 181
+|#
